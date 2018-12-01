0b175a8cf060b198734eee12beb16ef536cb80c0
akisai <akisai.azu@gmail.com>
2018-12-01 18:12:15 +0300

    Fix manifest for permissions

--- app/src/main/java/com/example/haimin_a/crm_test/SignInActivity.kt
+++ app/src/main/java/com/example/haimin_a/crm_test/SignInActivity.kt
@@ -1,10 +1,9 @@
 package com.example.haimin_a.crm_test
 
-import android.content.ContentProvider
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
