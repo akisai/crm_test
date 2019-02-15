a9151a3199c44545add152caf80f5536bd7287cc
Khaymin-a <akisai.azu@gmail.com>
2019-02-15 14:57:02 +0300

    tablayout fragment

--- app/src/main/java/com/example/haimin_a/crm_test/nav_fragments/GalleryFragment.kt
+++ app/src/main/java/com/example/haimin_a/crm_test/nav_fragments/GalleryFragment.kt
@@ -2,11 +2,13 @@ package com.example.haimin_a.crm_test.nav_fragments
 
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.haimin_a.crm_test.R
+import kotlinx.android.synthetic.main.fragment_gallery.*
 
 class GalleryFragment : Fragment() {
 
@@ -19,7 +21,28 @@ class GalleryFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+
+        tabLayout.addOnTabSelectedListener( object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+                when(p0?.text) {
+                    "Photo" -> activity!!.supportFragmentManager.beginTransaction()
+                        .replace(
+                            R.id.navigation_content,
+                            ProfileFragment()
+                        ).commit()
+                }
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+
+            }
+        })
 //        val intent = Intent(Intent.ACTION_PICK)
+//        val intent = Intent(Intent.ACTION_VIEW)
 //        intent.type = "video/*"
 //        startActivityForResult(intent, 11)
     }
---
+++ app/src/main/java/com/example/haimin_a/crm_test/nav_fragments/adapter/GalleryPageAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.haimin_a.crm_test.nav_fragments.adapter
+
+class GalleryPageAdapter {
+}
